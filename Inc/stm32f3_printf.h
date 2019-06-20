cf8f54122ab47f4384fdac2703106f2329b24659
Hajime Tamura <hajime0316@s.okayama-u.ac.jp>
2019-06-21 03:30:10 +0900

    Add extern C syntax

--- Inc/stm32f3_printf.h
+++ Inc/stm32f3_printf.h
@@ -11,8 +11,16 @@
 #include "stm32f3xx_hal.h"
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void stm32f3_printf_init(UART_HandleTypeDef *huart);
 void stm32f3_puts(uint8_t USART_TX_data[]);
 void stm32f3_printf(const char *format, ...);
+
+#ifdef __cplusplus
+};
+#endif
 
 #endif /* UARTPRINTF_H_ */
