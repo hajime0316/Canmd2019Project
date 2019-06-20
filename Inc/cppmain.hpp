8a9c4827e9c7945c412eb1dee902a348860e80e7
Hajime Tamura <hajime0316@s.okayama-u.ac.jp>
2019-06-21 02:08:50 +0900

    Add cppmain.hpp

---
+++ Inc/cppmain.hpp
@@ -0,0 +1,15 @@
+#ifndef CPPMAIN_HPP_
+#define CPPMAIN_HPP_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void setup(void);
+void loop(void);
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif /* CPPMAIN_HPP_ */
