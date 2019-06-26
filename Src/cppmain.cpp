8e8188530e3b385827ce12afaa54d7cf14fd9a19
Hajime Tamura <hajime0316@s.okayama-u.ac.jp>
2019-06-27 01:14:46 +0900

    Reverse branch order

--- Src/cppmain.cpp
+++ Src/cppmain.cpp
@@ -110,7 +110,14 @@ void stm32f3_easy_can_interrupt_handler(void)
     transmit_id = md_id   << 5 | 0b00000 ;
     //            送信元ID(5bit)  送信先ID(5bit)
 
-    if(receive_md_data_type == MD_DATA_TYPE_CONTROL_DATA) {
+    if(receive_md_data_type != MD_DATA_TYPE_CONTROL_DATA) {
+        // 受信メッセージをそのまま送信メッセージとする
+        transmit_dlc = receive_dlc;
+        for(int i = 0; i < receive_dlc; i++) {
+            transmit_message[i] = receive_message[i];
+        }
+    }
+    else {
         // エンコーダのカウント値を送信メッセージとする
         static Stm32f3Velocity divided_encoder_count[2] = {&htim2, &htim3};
         for(int i = 0; i < 2; i++) {
@@ -122,13 +129,6 @@ void stm32f3_easy_can_interrupt_handler(void)
                             | (divided_encoder_count[1].get_velocity >> 8 & 0b111   );
         transmit_message[1] = divided_encoder_count[0].get_velocity & 0XFF;
         transmit_message[2] = divided_encoder_count[1].get_velocity & 0XFF;
-    }
-    else {
-        // 受信メッセージをそのまま送信メッセージとする
-        transmit_dlc = receive_dlc;
-        for(int i = 0; i < receive_dlc; i++) {
-            transmit_message[i] = receive_message[i];
-        }
     }
 
     // データ送信
