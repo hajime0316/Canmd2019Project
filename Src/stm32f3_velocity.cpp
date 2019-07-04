48b03a8fb19c32f6ee5a299da4a01323585ea123
Hajime Tamura <hajime0316@s.okayama-u.ac.jp>
2019-07-04 22:52:42 +0900

    Add HAL_TIM_Encoder_Start function
    エンコーダのカウントをスタートさせる関数を，velocityモジュールの関
    数内でコールするようにした．

--- Src/stm32f3_velocity.cpp
+++ Src/stm32f3_velocity.cpp
@@ -21,6 +21,9 @@ Stm32f3Velocity::Stm32f3Velocity(const TIM_HandleTypeDef *htim) {
     this->htim = htim;
     this->velocity = 0;
     this->past_encoder_count = htim->Instance->CNT;
+    
+    // ハードウェアスタート
+    HAL_TIM_Encoder_Start((TIM_HandleTypeDef *)htim, TIM_CHANNEL_ALL);
 }
 
 Stm32f3Velocity::~Stm32f3Velocity() {
