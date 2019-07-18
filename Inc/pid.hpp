289e42d4eb4a111e4e4b58882a56e9118aa1d489
Siva0410 <kaitofffff@gmail.com>
2019-07-18 11:53:24 +0900

    add diff_data and prev_diff_data

--- Inc/pid.hpp
+++ Inc/pid.hpp
@@ -17,20 +17,28 @@ class Pid {
     public:
         Pid();
         ~Pid();
-        //各誤差の宣言
+        //各誤差
         double p_error=0;
         double i_error=0;
         double d_error=0;
 
-        //ゲインの宣言
+        //ゲイン
         double Kp=0;
         double Ki=0;
         double Kd=0;
-        //目的のデータの宣言
+
+        //目的のデータ
         double ideal_data=0;
-        //エンコーダの値を保持する変数の宣言
+
+        //エンコーダの値を保持する変数
         double enc=0;
 
+        //目的とエンコーダの値の差を保持する変数
+        double diff_data = ideal_data - enc;
+
+        //前のdiff_dataの値を保持する変数
+        double prev_diff_data=0;
+        
         //ゲインの取得
         void set_gain(double, double, double);
 
@@ -42,7 +50,7 @@ class Pid {
         
         //誤差の更新
         void update_errors();
-        
+
         //PID処理
         double pid_cal();
 };
